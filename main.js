06a6b9ba5600df47eb69f5abaf2c1729e52145f8
felixhelix11 <124505467+felixhelix11@users.noreply.github.com>
2025-02-13 23:42:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+// Play audio function
+function playAudio() {
+    const audio = document.getElementById("valentineAudio");
+    if (audio.paused) {
+        audio.play(); // Play the audio if it's paused
+    }
+}
+
+function toggleDiv1122() {
+    // Play the audio when the heart is clicked
+    playAudio();
+
+    // Toggle visibility of div11 and div22
+    var div11 = document.getElementById('div11');
+    var div22 = document.getElementById('div22');
+
+    if (div11.style.display !== 'none') {
+        div11.style.display = 'none';
+        div22.style.display = 'block';
+    } else {
+        div11.style.display = 'block';
+        div22.style.display = 'none';
+    }
+}
+
+function toggleDiv0() {
+    // Ensure audio keeps playing
+    playAudio();
+
+    var div0 = document.getElementById('div0');
+    var div1 = document.getElementById('div1');
+
+    if (div0.style.display !== 'none') {
+        div0.style.display = 'none';
+        div1.style.display = 'block';
+    } else {
+        div0.style.display = 'block';
+        div1.style.display = 'none';
+    }
+}
+
+function toggleDiv1() {
+    // Ensure audio keeps playing
+    playAudio();
+
+    var div1 = document.getElementById('div1');
+    var div2 = document.getElementById('div2');
+
+    if (div1.style.display !== 'none') {
+        div1.style.display = 'none';
+        div2.style.display = 'block';
+    } else {
+        div1.style.display = 'block';
+        div2.style.display = 'none';
+    }
+}
+
+function increaseSize() {
+    // Ensure audio keeps playing
+    playAudio();
+
+    var button1 = document.getElementById('btn-yes');
+    var maxSize = 500;
+    var currentSize = parseInt(window.getComputedStyle(button1).getPropertyValue('padding'));
+
+    function increaseButtonSize() {
+        currentSize += 30;
+        if (currentSize < maxSize) {
+            button1.style.padding = currentSize + 'px';
+        } else {
+            resetButtonSize();
+        }
+    }
+    increaseButtonSize();
+}
+
+function resetButtonSize() {
+    var button = document.getElementById('btn-yes');
+    button.style.padding = '8px';
+}
+
+function toggleDiv2() {
+    // Ensure audio keeps playing
+    playAudio();
+
+    var div2 = document.getElementById('div2');
+    var div3 = document.getElementById('div3');
+    var hearts = document.getElementById('heart-float');
+
+    if (div2.style.display !== 'none') {
+        div2.style.display = 'none';
+        div3.style.display = 'block';
+        hearts.style.display = 'block';
+    } else {
+        div2.style.display = 'block';
+        div3.style.display = 'none';
+        hearts.style.display = 'none';
+    }
+}
